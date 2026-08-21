acc02cdb9a4edc5f07b75697f719f22328fec1ea
Mr. Thorne <85703541+Ye-Shwethway@users.noreply.github.com>
2026-08-21 12:12:36 +0630

    Support Telegram callback queries

--- src/telegram/types.ts
+++ src/telegram/types.ts
@@ -13,7 +13,15 @@ export interface TelegramMessage {
   text?: string;
 }
 
+export interface TelegramCallbackQuery {
+  id: string;
+  from: TelegramUser;
+  data?: string;
+  message?: TelegramMessage;
+}
+
 export interface TelegramUpdate {
   update_id: number;
   message?: TelegramMessage;
+  callback_query?: TelegramCallbackQuery;
 }
